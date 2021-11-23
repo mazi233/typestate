2f8850ceb6e9b3e035ec78c0d748a4ba3f10e6fa
mazi <602281666@qq.com>
2021-11-23 14:59:57 +0800

    泛型构建器

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     id: String,
     email: String,
@@ -14,28 +15,54 @@ impl User {
     //     Self { email, first_name, last_name }
     // }
 
-    fn builder(id: impl Into<String>, email: impl Into<String>) -> UserBuilder {
-        UserBuilder::new(id, email)
+    fn builder() -> UserBuilder<NoId, NoEmail> {
+        UserBuilder::new()
     }
 }
 
-struct UserBuilder {
-    id: String,
-    email: String,
+struct UserBuilder<I, E> {
+    id: I,
+    email: E,
     first_name: Option<String>,
     last_name: Option<String>
 }
 
-impl UserBuilder {
-    fn new(id: impl Into<String>, email: impl Into<String>) -> Self {
+impl UserBuilder<NoId, NoEmail> {
+    fn new() -> Self {
         Self {
-            id: id.into(),
-            email: email.into(),
+            id: NoId,
+            email: NoEmail,
             first_name: None,
             last_name: None,
         }
     }
+}
 
+impl<E> UserBuilder<NoId, E> {
+    fn id(self, id: impl Into<String>) -> UserBuilder<Id, E> {
+        let Self { email, first_name, last_name, .. } = self;
+        UserBuilder {
+            id: Id(id.into()),
+            email,
+            first_name,
+            last_name,
+        }
+    }
+}
+
+impl<I> UserBuilder<I, NoEmail> {
+    fn email(self, email: impl Into<String>) -> UserBuilder<I, Email> {
+        let Self { id, first_name, last_name, .. } = self;
+        UserBuilder {
+            id,
+            email: Email(email.into()),
+            first_name,
+            last_name,
+        }
+    }
+}
+
+impl<I, E> UserBuilder<I, E> {
     fn first_name(mut self, first_name: impl Into<String>) -> Self {
         self.first_name = Some(first_name.into());
         self
@@ -45,15 +72,33 @@ impl UserBuilder {
         self.last_name = Some(last_name.into());
         self
     }
+}
 
+impl UserBuilder<Id, Email> {
     fn build(self) -> User {
         let Self { id, email, first_name, last_name } = self;
-        User { id, email, first_name, last_name }
+        User {
+            id: id.0,
+            email: email.0,
+            first_name,
+            last_name,
+        }
     }
 }
 
+struct Id(String);
+struct NoId;
+
+struct Email(String);
+struct NoEmail;
+
 fn main() {
-    let greyblake = User::builder("13", "greyblake@example.com")
-        .first_name("Sergey")
+    let greyblake = User::builder()
+        .email("602281666@qq.com")
+        .id("13")
+        .first_name("ma")
+        .last_name("zi")
         .build();
+    
+    println!("{:?}", greyblake);
 }
