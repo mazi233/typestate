7f2e8f9c35bb4615f8f42ad19aaca662915eec5b
mazi <602281666@qq.com>
2021-11-23 11:30:25 +0800

    init a struct use builder mode

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,57 @@ struct User {
 }
 
 impl User {
-    fn new(
-        email: Option<String>,
-        first_name: Option<String>,
-        last_name: Option<String>
-    ) -> Self {
-        Self { email, first_name, last_name }
+    // fn new(
+    //     email: Option<String>,
+    //     first_name: Option<String>,
+    //     last_name: Option<String>
+    // ) -> Self {
+    //     Self { email, first_name, last_name }
+    // }
+    fn builder() -> UserBuilder {
+        UserBuilder::new()
+    }
+}
+
+struct UserBuilder {
+    email: Option<String>,
+    first_name: Option<String>,
+    last_name: Option<String>
+}
+
+impl UserBuilder {
+    fn new() -> Self {
+        Self {
+            email: None,
+            first_name: None,
+            last_name: None,
+        }
+    }
+
+    fn email(mut self, email: impl Into<String>) -> Self {
+        self.email = Some(email.into());
+        self
+    }
+
+    fn first_name(mut self, first_name: impl Into<String>) -> Self {
+        self.first_name = Some(first_name.into());
+        self
+    }
+
+    fn last_name(mut self, last_name: impl Into<String>) -> Self {
+        self.last_name = Some(last_name.into());
+        self
+    }
+
+    fn build(self) -> User {
+        let Self { email, first_name, last_name } = self;
+        User { email, first_name, last_name }
     }
 }
 
 fn main() {
-    let greyblake = User::new(
-        Some("example@example.com".to_string()),
-        Some("Sergey".to_string()),
-        None,
-    );
+    let greyblake = User::builder()
+        .email("example@example.com")
+        .first_name("Sergey")
+        .build();
 }
