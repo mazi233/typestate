63f87e42dd10ab102cfd32f055ccf3ac4dd827e3
mazi <602281666@qq.com>
2021-11-23 11:26:28 +0800

    init a struct use new method

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,20 @@ struct User {
     last_name: Option<String>
 }
 
+impl User {
+    fn new(
+        email: Option<String>,
+        first_name: Option<String>,
+        last_name: Option<String>
+    ) -> Self {
+        Self { email, first_name, last_name }
+    }
+}
+
 fn main() {
-    let greyblake = User {
-        email: Some("example@example.com".to_string()),
-        first_name: Some("Sergey".to_string()),
-        last_name: None,
-    };
+    let greyblake = User::new(
+        Some("example@example.com".to_string()),
+        Some("Sergey".to_string()),
+        None,
+    );
 }
